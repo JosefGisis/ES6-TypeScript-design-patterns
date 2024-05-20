9b3dedb0f4ef7cc4f9450fb26b9ae5d8617a4b1c
josef gisis <jsquared.gisis@gmail.com>
2024-05-20 12:27:46 -0400

    created abstractFactory pattern

---
+++ README.md
@@ -0,0 +1,8 @@
+This repo contains practice examples for all 23 design patterns from "Design Patterns: Elements of Reusable Object-Oriented Software" by Gamma, Helm, Johnson, and Vlissides.
+
+The examples are contained within directories ordered by the design pattern's purpose:
+1. Creational: the way objects are created.
+2. Structural: the way objects relate to each other.
+3. Behavioral: the way objects communicate with each other.
+
+- Josef Gisis 05/20/2024
---
+++ src/creational/abstractFactory.ts
@@ -0,0 +1,96 @@
+// The abstract factory pattern provides an interface for creating related or dependant objects
+// without specifying their concrete classes. This allows the client to be separated from the
+// specifics of the object conformities and dependencies.
+
+// Example using es6 features.
+
+type Shoes = 'red shoes' | 'blue shoes'
+type Shirt = 'red shirt' | 'blue shirt'
+type Pants = 'red pants' | 'blue pants'
+
+// Abstract factory and concrete factories.
+abstract class AbstractClothingFactory {
+	abstract createShoes(): Shoes
+
+	abstract createShirt(): Shirt
+
+	abstract createPants(): Pants
+}
+
+class ConcreteRedClothingFactory extends AbstractClothingFactory {
+	createShoes(): Shoes {
+		return new RedShoes().createRedProduct()
+	}
+
+	createShirt(): Shirt {
+		return new RedShirt().createRedProduct()
+	}
+
+	createPants(): Pants {
+		return new RedPants().createRedProduct()
+	}
+}
+
+class ConcreteBlueClothingFactory extends AbstractClothingFactory {
+	createShoes(): Shoes {
+		return new BlueShoes().createBlueProduct()
+	}
+
+	createShirt(): Shirt {
+		return new BlueShirt().createBlueProduct()
+	}
+
+	createPants(): Pants {
+		return new BluePants().createBlueProduct()
+	}
+}
+
+// Abstract products and concrete products.
+abstract class AbstractRedClothingProduct {
+	abstract createRedProduct(): string
+}
+
+class RedShoes extends AbstractRedClothingProduct {
+	override createRedProduct(): 'red shoes' {
+		return 'red shoes'
+	}
+}
+
+class RedShirt extends AbstractRedClothingProduct {
+	override createRedProduct(): 'red shirt' {
+		return 'red shirt'
+	}
+}
+
+class RedPants extends AbstractRedClothingProduct {
+	override createRedProduct(): 'red pants' {
+		return 'red pants'
+	}
+}
+
+abstract class AbstractBlueClothingProduct {
+	abstract createBlueProduct(): string
+}
+
+class BlueShoes extends AbstractBlueClothingProduct {
+	override createBlueProduct(): 'blue shoes' {
+		return 'blue shoes'
+	}
+}
+
+class BlueShirt extends AbstractBlueClothingProduct {
+	override createBlueProduct(): 'blue shirt' {
+		return 'blue shirt'
+	}
+}
+
+class BluePants extends AbstractBlueClothingProduct {
+	override createBlueProduct(): 'blue pants' {
+		return 'blue pants'
+	}
+}
+
+// Usage
+const factory = new ConcreteRedClothingFactory()
+const shoes = factory.createShoes()
+console.log(shoes)
